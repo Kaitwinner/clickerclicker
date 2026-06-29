cdc24d0e76de90a45af6e2b5d21a8c9934b39fde
Kait-e <Kaitevander@gmail.com>
2026-06-30 01:28:26 +0300

    REMADE UPGRADES FOR SCALABILITY LETS GOOO

--- clickclick/scripts/cookiegame.gd
+++ clickclick/scripts/cookiegame.gd
@@ -7,45 +7,32 @@ func _ready() -> void:
 	$fade_anim/AnimationPlayer.play("fade_in")
 	$fade_anim/AnimationPlayer.animation_finished.connect(_on_fade_finished)
 	Input.mouse_mode = Input.MOUSE_MODE_CONFINED
+	for upgrade_name in UPGRADES:
+		var btn = $VBoxContainer2.get_node(upgrade_name)
+		btn.pressed.connect(_on_upgrade_pressed.bind(upgrade_name))
 	_update_ui()
 
 func _on_fade_finished(_anim_name: StringName) -> void:
 	$fade_anim.mouse_filter = ColorRect.MOUSE_FILTER_IGNORE
 
 func _on_cookiebutton_button_down() -> void:
-	cookies *= amount_per_click
+	cookies += amount_per_click
 	_update_ui()
 
 func _update_ui() -> void:
 	$VBoxContainer/Label.text = str(cookies) + " cookies"
 
-func _on_Upgrade_01_pressed() -> void:
-	if cookies >= 10:
-		cookies -= 10
-		amount_per_click += 1
-		$VBoxContainer2/Upgrade_01.queue_free()
-		_update_ui()
+const UPGRADES = {
+	"Upgrade_01": {"cost": 50,  "bonus": 1},
+	"Upgrade_02": {"cost": 200, "bonus": 2},
+	"Upgrade_03": {"cost": 200, "bonus": 2},
+	"Upgrade_04": {"cost": 1500, "bonus": 15},
+}
 
-
-func _on_upgrade_02_pressed() -> void:
-	if cookies >= 100:
-		cookies -= 100
-		amount_per_click += 8
-		$VBoxContainer2/Upgrade_02.queue_free()
-		_update_ui()
-
-
-func _on_upgrade_03_pressed() -> void:
-	if cookies >= 200:
-		cookies -= 200
-		amount_per_click += 15
-		$VBoxContainer2/Upgrade_03.queue_free()
-		_update_ui()
-
-
-func _on_upgrade_04_pressed() -> void:
-	if cookies >= 500:
-		cookies -= 500
-		amount_per_click += 75
-		$VBoxContainer2/Upgrade_04.queue_free()
+func _on_upgrade_pressed(upgrade_name: String) -> void:
+	var upgrade = UPGRADES[upgrade_name]
+	if cookies >= upgrade.cost:
+		cookies -= upgrade.cost
+		amount_per_click += upgrade.bonus
+		$VBoxContainer2.get_node(upgrade_name).queue_free()
 		_update_ui()
